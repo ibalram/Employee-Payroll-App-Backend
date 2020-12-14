88cddf160a580b6a766e0796e952cf0a6dbe2f38
ibalram <balramsinghrathore3@gmail.com>
2020-12-15 01:21:24 +0530

    Added: Use of Slf4j logger and Application.properties

--- src/main/java/com/cg/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/cg/employeepayrollapp/EmployeepayrollappApplication.java
@@ -3,7 +3,11 @@ package com.cg.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeepayrollappApplication {
 
 	public static void main(String[] args) {
